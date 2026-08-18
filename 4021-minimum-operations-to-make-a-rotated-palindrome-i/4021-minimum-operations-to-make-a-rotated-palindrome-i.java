bbad6f32104b428afdef5a315eb74cb66b1a9c88
krrishkumar31072006-lgtm <krrishkumar31072006@gmail.com>
2026-08-18 15:08:34 +0530

    Time: 292 ms (84.6%), Space: 47.2 MB (6.08%) - LeetHub

---
+++ 4021-minimum-operations-to-make-a-rotated-palindrome-i/4021-minimum-operations-to-make-a-rotated-palindrome-i.java
@@ -0,0 +1,32 @@
+class Solution {
+    int fun(String s){
+        int i=0;
+        int j=s.length()-1;
+        int cost=0;
+
+        while(i<j){
+            
+                int a1 = Math.abs((s.charAt(i)-'a')-(s.charAt(j)-'a'));
+                int a2=26-a1;
+                cost += Math.min(a1,a2);
+            i++;
+            j--;
+        }
+        return cost;
+    }
+
+
+    public int minOperations(String s) {
+      StringBuilder sb= new StringBuilder(s);
+      int m = Integer.MAX_VALUE;
+
+             for(int i =0;i<sb.length();i++){
+                int cost= fun(sb.toString())+i;
+                m=Math.min(m,cost);
+            sb.append(sb.charAt(0));
+            sb.deleteCharAt(0);
+          
+       } 
+       return m;
+    }
+}
