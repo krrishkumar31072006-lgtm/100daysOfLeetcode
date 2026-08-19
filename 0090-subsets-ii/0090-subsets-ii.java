4e4aacdd188ce9dfd2616df33bf3fb00dcd0242d
krrishkumar31072006-lgtm <krrishkumar31072006@gmail.com>
2026-08-19 14:32:27 +0530

    Time: 13 ms (5.46%), Space: 45.3 MB (24.09%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,20 +1,24 @@
+//input output approach
+
 class Solution {
-    public void sub(List<List<Integer>> ans,int i,int nums[],List<Integer> out){
-        if(i >= nums.length){
-            if(!ans.contains(new ArrayList<>(out))){
-            ans.add(new ArrayList<>(out));
-            }
-            return;
-        }
-        sub(ans,i+1,nums,out);
-        out.add(nums[i]);
-        sub(ans,i+1,nums,out);
-        out.remove(out.size()-1);
+    public void sub(List<List<Integer>> ans,List<Integer> out,List<Integer> inp){
+        if(!ans.contains(out)) ans.add(new ArrayList<>(out));
+          if(inp.size() == 0) return ;
+          for(int i = 0;i < inp.size();i++){
+            List<Integer> newinp = new ArrayList<>(inp);
+            List<Integer> newout = new ArrayList<>(out);
+            newout.add(newinp.get(i));
+            newinp.subList(0,i+1).clear();
+            sub(ans,newout,newinp);
+          }
     }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> inp = new ArrayList<>();
         Arrays.sort(nums);
-        sub(ans,0,nums,new ArrayList<>());
+        for(int ele:nums) inp.add(ele);
+        sub(ans,new ArrayList<>(),inp);
         return ans;
+
     }
 }
