cf069f2c5679f611237a46b2eb3e53efdda1df93
krrishkumar31072006-lgtm <krrishkumar31072006@gmail.com>
2026-09-14 16:50:09 +0530

    Time: 12 ms (55.86%), Space: 44.1 MB (26.13%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -5,10 +5,10 @@ class Solution {
         if(s.length() != t.length()) return false;
         for(int i=0; i<s.length();i++){
             if(map.containsKey(s.charAt(i))){
-                if(map.get(s.charAt(i))==t.charAt(i)){
-                    map.put(s.charAt(i),t.charAt(i));
-                }else return false;
-            } else if(set.contains(t.charAt(i))){
+                if(map.get(s.charAt(i))!=t.charAt(i))
+                  return false;
+            }
+            else if(set.contains(t.charAt(i))){
                 return false;
             }else{
                 map.put(s.charAt(i),t.charAt(i));
